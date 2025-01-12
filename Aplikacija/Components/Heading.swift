469a9b71d577a960b103c04d59bc19887276bf7a
SaturnianCube <>
2025-01-13 00:57:33 +0100

    use `navigationTitle` for heading

--- Aplikacija/Components/Heading.swift
+++ Aplikacija/Components/Heading.swift
@@ -12,10 +12,8 @@ struct Heading: View {
 	@State var text: String
 	
     var body: some View {
-		HStack {
-			Text(text).font(.largeTitle).fontWeight(.bold)
-			Spacer()
-		}
+		Text("")
+			.navigationTitle(text)
     }
 }
 
