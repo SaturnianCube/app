296f073f1047d944aba5cb3c08bd981174fdfb7e
SaturnianCube <>
2025-01-12 22:32:26 +0100

    add ProfileView

---
+++ Aplikacija/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Aplikacija
+//
+//  Created on 12. 1. 25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("ProfileView")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
