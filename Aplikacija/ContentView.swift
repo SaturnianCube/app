891b418f83a7962933315760f8b2dee51caf7239
SaturnianCube <>
2025-01-12 22:31:50 +0100

    refactor ContentView

--- Aplikacija/ContentView.swift
+++ Aplikacija/ContentView.swift
@@ -12,60 +12,26 @@ struct ContentView: View {
 	
 	@ObservedObject var dataManager = DataManager.shared
 	
-	@State private var isSheetVisible: Bool = true
-	@State private var viewingEvent: Event? = nil
-	
 	var body: some View {
-		Map {
-			ForEach(dataManager.events) { event in
-				Marker(event.title, systemImage: event.type.getIcon(), coordinate: event.position)
-			}
-		}.sheet(isPresented: $isSheetVisible) {
-			VStack {
-				
-				Text("Iskana pomoč")
-					.font(.largeTitle)
-					.bold()
-					.frame(maxWidth: .infinity, alignment: .leading)
-				
-				ForEach($dataManager.events) { $event in
-					Button {
-						viewingEvent = event
-					} label: {
-						HStack {
-							Image(systemName: event.type.getIcon())
-								.resizable()
-								.scaledToFit()
-								.frame(width: 30, height: 30, alignment: .center)
-							Text("\(event.title)")
-								.frame(maxWidth: .infinity, alignment: .leading)
-						}
-						.frame(maxWidth: .infinity)
-						.padding([ .leading, .trailing ], 15)
-						.padding([ .top, .bottom ], 5)
-						.background(Color(UIColor.secondarySystemBackground))
-						.cornerRadius(5)
-					}
-				}
-				
-				Spacer()
-				
-				Button("Dodaj", systemImage: "plus") {
-					
-				}
-					.buttonStyle(.borderedProminent)
-					.font(.system(size: 25))
-					.fontWeight(.bold)
-					.padding([.top ], 30)
-			}
-				.padding([ .top ], 20)
-				.padding([ .leading, .trailing ], 15)
-				.presentationDetents([ .medium, .large ])
-				.interactiveDismissDisabled()
+		TabView {
+			let _ = print("\(dataManager.events.count)")
 			
-			.sheet(item: $viewingEvent) { event in
-				EventInfo(event: event)
-			}
+			MapView()
+				.tabItem {
+					Label("Zemljevid", systemImage: "mappin.and.ellipse")
+				}
+				.badge(dataManager.events.count )
+			
+			EventSearchView()
+				.tabItem {
+					Label("Iskalnik", systemImage: "magnifyingglass")
+				}
+			
+			ProfileView()
+				.tabItem {
+					Label("Profil", systemImage: "person")
+				}
+			
 		}
 	}
 
@@ -74,3 +40,4 @@ struct ContentView: View {
 #Preview {
 	ContentView()
 }
+
