693fabf6234a75ee4377cef7d1fdbdaa3dc2fb76
SaturnianCube <>
2025-01-18 05:11:25 +0100

    use task modifier

--- Aplikacija/Views/ContentView.swift
+++ Aplikacija/Views/ContentView.swift
@@ -31,11 +31,10 @@ struct ContentView: View {
 					Label("Profil", systemImage: "person")
 				}
 			
-		}.onAppear {
-			Task {
-				dataManager.currentUser = await User.fetchById(id: "EVUYpzqMpbzMjCBwA6Fd")
-				dataManager.events = await Event.fetchAll()
-			}
+		}
+		.task {
+			dataManager.currentUser = await User.fetchById(id: "EVUYpzqMpbzMjCBwA6Fd")
+			dataManager.events = await Event.fetchAll()
 		}
 	}
 
